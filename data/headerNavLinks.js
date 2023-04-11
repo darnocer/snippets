cc9f66d74f1d0a02e2f575fbf745301d8f4ad770
Darian Nocera <darian@Darians-MacBook-Pro.local>
2023-04-11 13:10:58 -0600

    modified starter

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,9 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  // { href: '/blog', title: 'Blog' },
+  // { href: '/tags', title: 'Tags' },
+  // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: 'https://darian.digital', title: 'Hire Me' },
 ]
 
 export default headerNavLinks
